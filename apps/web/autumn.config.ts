03394092d05441a0deeeae47ca93964cbf77c685
Ben Davis <davis.benjamin41902@gmail.com>
2026-01-21 01:46:14 -0800

    atmn

--- apps/web/autumn.config.ts
+++ apps/web/autumn.config.ts
@@ -1,46 +1,68 @@
 import { feature, product, featureItem, priceItem } from 'atmn';
-import { BILLING_PLAN, FEATURE_IDS } from './src/lib/billing/plans.ts';
 
-export const tokensIn = feature({
-	id: FEATURE_IDS.tokensIn,
-	name: 'Tokens In',
-	type: 'single_use'
-});
-
-export const tokensOut = feature({
-	id: FEATURE_IDS.tokensOut,
-	name: 'Tokens Out',
-	type: 'single_use'
-});
-
+// Features
 export const sandboxHours = feature({
-	id: FEATURE_IDS.sandboxHours,
+	id: 'sandbox_hours',
 	name: 'Sandbox Hours',
 	type: 'single_use'
 });
 
-export const proPlan = product({
-	id: BILLING_PLAN.id,
-	name: `${BILLING_PLAN.name} Plan`,
+export const tokensOut = feature({
+	id: 'tokens_out',
+	name: 'Tokens Out',
+	type: 'single_use'
+});
+
+export const tokensIn = feature({
+	id: 'tokens_in',
+	name: 'Tokens In',
+	type: 'single_use'
+});
+
+export const chatMessages = feature({
+	id: 'chat_messages',
+	name: 'Chat Messages',
+	type: 'single_use'
+});
+
+// Products
+export const freePlan = product({
+	id: 'free_plan',
+	name: 'Free Plan',
+	is_default: true,
+	items: [
+		featureItem({
+			feature_id: chatMessages.id,
+			included_usage: 5
+		})
+	]
+});
+
+export const btcaPro = product({
+	id: 'btca_pro',
+	name: 'Pro Plan',
 	items: [
 		priceItem({
-			price: BILLING_PLAN.priceUsd,
-			interval: BILLING_PLAN.interval
+			price: 8,
+			interval: 'month'
 		}),
-		featureItem({
-			feature_id: tokensIn.id,
-			included_usage: BILLING_PLAN.limits.tokensIn,
-			interval: BILLING_PLAN.interval
-		}),
-		featureItem({
-			feature_id: tokensOut.id,
-			included_usage: BILLING_PLAN.limits.tokensOut,
-			interval: BILLING_PLAN.interval
-		}),
+
 		featureItem({
 			feature_id: sandboxHours.id,
-			included_usage: BILLING_PLAN.limits.sandboxHours,
-			interval: BILLING_PLAN.interval
+			included_usage: 6,
+			interval: 'month'
+		}),
+
+		featureItem({
+			feature_id: tokensIn.id,
+			included_usage: 1500000,
+			interval: 'month'
+		}),
+
+		featureItem({
+			feature_id: tokensOut.id,
+			included_usage: 300000,
+			interval: 'month'
 		})
 	]
 });
